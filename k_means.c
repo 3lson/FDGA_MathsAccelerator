3b08336e8bd13b866f1b8c2e27718ae39984baad
Antoni Steglinski <antoni.steglinski@gmail.com>
2025-05-23 15:59:58 +0200

    Update k_means.c
    deleted unnecessary functions

--- k_means.c
+++ k_means.c
@@ -12,61 +12,13 @@ struct Point {
 float fabsf(float x) {
     return x < 0 ? -x : x;
 }
-// Manhattan distance between two points
-float MDistance(struct Point a, struct Point b) {
-    return fabsf(a.x - b.x) + fabsf(a.y - b.y);
-}
-
-// K-means clustering using Manhattan distance
-void Clustering(struct Point centroids[], struct Point points[], int num_points, struct Point clusters[][MAX_POINTS], int cluster_sizes[], int k) {
-    for (int i = 0; i < k; i++) {
-        cluster_sizes[i] = 0;
-    }
-
-    for (int i = 0; i < num_points; i++) {
-        float min_dist = MDistance(centroids[0], points[i]);
-        int best = 0;
-
-        for (int j = 1; j < k; j++) {
-            float d = MDistance(centroids[j], points[i]);
-            if (d < min_dist) {
-                min_dist = d;
-                best = j;
-            }
-        }
-
-        clusters[best][cluster_sizes[best]++] = points[i];
-    }
-}
-
-// recalculate centroids
-int UpdateCentroids(struct Point centroids[], struct Point clusters[][MAX_POINTS], int cluster_sizes[], int k) {
-    for (int i = 0; i < k; i++) {
-        float sum_x = 0, sum_y = 0;
-        int size = cluster_sizes[i];
-        for (int j = 0; j < size; j++) {
-            sum_x += clusters[i][j].x;
-            sum_y += clusters[i][j].y;
-        }
-
-        if (size > 0) {
-            centroids[i].x = sum_x / size;
-            centroids[i].y = sum_y / size;
-        }
-    }
-    return 0;
-}
-
-//Had to add that cause != gave me wrong results sometimes with floats chat said that it is because of floating point precision issues
-int Equal(struct Point a, struct Point b) {
-    float EPS = 0.000001;
-    return (fabsf(a.x - b.x) < EPS) && (fabsf(a.y - b.y) < EPS);
-}
 
 int main() {
+    float EPS = 0.000001;
     int k;
     struct Point centroids[MAX_K];
     struct Point points[MAX_POINTS];
+    struct Point old_centroids[MAX_K];
     int num_points;
 
     printf("Enter number of centroids: ");
@@ -90,15 +42,47 @@ int main() {
 
     int max_iter = 100;
     for (int cycle = 0; cycle < max_iter; cycle++) {
-        struct Point old_centroids[MAX_K];
 
         for (int i = 0; i < k; i++) old_centroids[i] = centroids[i];
-        Clustering(centroids, points, num_points, clusters, cluster_sizes, k);
-        UpdateCentroids(centroids, clusters, cluster_sizes, k);
+
+        // updated clustering functions
+        for (int i = 0; i < k; i++) {
+            cluster_sizes[i] = 0;
+        }
+
+        for (int i = 0; i < num_points; i++) {
+            float min_dist = fabsf(centroids[0].x - points[i].x) + fabsf(centroids[0].y - points[i].y);
+            int best = 0;
+
+            for (int j = 1; j < k; j++) {
+                float d = fabsf(centroids[j].x - points[i].x) + fabsf(centroids[j].y - points[i].y);
+                if (d < min_dist) {
+                    min_dist = d;
+                    best = j;
+                }
+            }
+
+            clusters[best][cluster_sizes[best]++] = points[i];
+        }
+        //updated centroids function
+        for (int i = 0; i < k; i++) {
+            float sum_x = 0, sum_y = 0;
+            int size = cluster_sizes[i];
+            for (int j = 0; j < size; j++) {
+                sum_x += clusters[i][j].x;
+                sum_y += clusters[i][j].y;
+            }
+
+            if (size > 0) {
+                centroids[i].x = sum_x / size;
+                centroids[i].y = sum_y / size;
+            }
+    }
         int done = 1;
 
         for (int i = 0; i < k; i++) {
-            if (!Equal(centroids[i], old_centroids[i])) {
+            if (!((fabsf(centroids[i].x - old_centroids[i].x) < EPS) && (fabsf(centroids[i].y - old_centroids[i].y) < EPS))){
+
                 done = 0;
                 break;
             }
